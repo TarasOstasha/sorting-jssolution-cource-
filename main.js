5181c77205ecb659cd1089f6cd6a740250fac524
TarasOstasha <tonyjoss1990@gmail.com>
2017-02-22 19:38:01 +0200

    fixed bug in sotingDown and done

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-                        
-                        //--realeaze function bubbleSort--//
+//--realeaze function bubbleSort--//
 
 var numbers = [1, 1000, 30000, 550, 6, 100, 51, 10, 1000000];
 var btn = document.getElementsByClassName('btnSuccess')[0];
@@ -7,44 +6,31 @@ btn.addEventListener('click', function bubbleSort() {
     var changet;
     do {
         changet = false;
-        for (var i=0; i < numbers.length-1; i++) {
-            if (numbers[i] > numbers[i+1]) {
+        for (var i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] > numbers[i + 1]) {
                 var temp = numbers[i];
-                numbers[i] = numbers[i+1];
-                numbers[i+1] = temp;
+                numbers[i] = numbers[i + 1];
+                numbers[i + 1] = temp;
                 changet = true;
             }
         }
     } while (changet);
- alert(numbers); 
+    alert(numbers);
 });
 
+var jsonData;
 
-
-
-
-
-                //--realeaze function Selection Sort--//
-
-var xhr = new XMLHttpRequest();
-xhr.open("get", "/dataFeb-21-2017.json");
-xhr.responseType = "json";
-xhr.addEventListener("readystatechange", function() {
-    if (this.readyState !== 4) {
-        return;
-    }
-    var beforeTable = document.getElementById('before');
-    outer(this.response, beforeTable);
-
-    var afterTable = document.getElementById('after');
-    outer(this.response, afterTable);
-    // code...
-    //this.response // <- json loaded from server
+//--realeaze function Selection Sort--//
+$.getJSON("dataFeb-21-2017.json", function (json) {
+    jsonData = json;
+    outer(json, 'before');
+    outer(json, 'after');
 });
-xhr.send();
 
-function outer(data, table) {
-    for( var i = 0; i < data.length; i++) {
+function outer(data, tableName) {
+    var table = document.getElementById(tableName);
+    clearTable(table)
+    for (i = 0; i < data.length; i++) {
         var tr = document.createElement('tr');
         var tdNames = document.createElement('td');
         var tdCity = document.createElement('td');
@@ -62,28 +48,56 @@ function outer(data, table) {
     }
 }
 
-var InsertionSort =  document.getElementById('btnsort');
-InsertionSort.addEventListener('click', function sort(data) {
+function clearTable(table) {
+    while (table.rows.length > 0) {
+        table.deleteRow(0);
+    }
+}
+
+var InsertionSortUp = document.getElementById('btnsortUp');
+InsertionSortUp.addEventListener('click', sortAsc);
+
+var InsertionSortDown = document.getElementById('btnsortDown');
+InsertionSortDown.addEventListener('click', sortDesc);
+
+//по спаданню
+function sortDesc() {
+    var data = jsonData;
     var len = data.length,
-    min, i, j;
-    for(i = 0; i < len; i++) {
+        i, j, val;
+    for (i = 0; i < len; i++) {
+        val = data[i];
+
+        for (j = i - 1; j > -1 && data[j].Names.localeCompare(val.Names) == -1; j--) {
+            data[j + 1] = data[j];
+        }
+
+        data[j + 1] = val;
+    }
+
+    outer(data, 'after');
+}
+
+//по зростанню
+function sortAsc() {
+    var data = jsonData;
+    var len = data.length,
+        min, i, j;
+    for (i = 0; i < len; i++) {
         min = i; //Мінімальний набір для цієї позиції
         //перевірити решту масиву, щоб побачити, чи ще є щось менше
-        for(j = i + 1; j < len; j++) {
-            if(data[j] < data[min]) {
+        for (j = i + 1; j < len; j++) {
+            if (data[j].Names.localeCompare(data[min].Names) == -1) {
                 min = j;
             }
         }
         //якщо мінімум не на позиції, поміняти його
-        if(i != min) {
+        if (i != min) {
             swap(data, i, min);
         }
     }
-    return data;
-});
-
-
-
+    outer(data, 'after');
+}
 
 
 function swap(data, firstIndex, secondIndex) {
